25f21f150d72b07b368ab496c30cd582bcd05f0e
amarav <amara.sudha@gmail.com>
2020-11-03 11:36:39 +0530

    exception issue

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import React from 'react';
-import { View , Text } from 'react-native';
-import AddEntry from './components/AddEntry';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import React, { Component } from 'react'
+import {View, StatusBar } from 'react-native'
+import { Constants } from 'react-native-unimodules'
 
-export default class App extends React.Component {
-  store = createStore(reducer);
-	render() {
-		return (
-			<View>
-			<Provider store={this.store}>
-				<View style={{ flex: 1 }}>
-					<AddEntry />
-				</View>
-			</Provider>
-			</View>
-		);
-	}
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+
+import AddEntry from './components/AddEntry'
+import History from './components/History'
+import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
+import { gray, orange, purple, white } from './utils/colors'
+
+import {NavigationContainer} from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import {setLocalNotifications } from './utils/helpers'
+
+
+
+function UdaciStatusBar({backgroundColor, ...props}){
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tab = createMaterialTopTabNavigator();
+function TabNaviagtor(){
+  return (
+    <Tab.Navigator 
+      tabBarOptions={{
+        activeTintColor: white, 
+        inactiveTintColor: gray,
+        style: {backgroundColor: purple},
+        tabStyle: {height: 54},
+        indicatorStyle: {backgroundColor: orange}
+      }}>
+      <Tab.Screen name="History" component={History}/>
+      <Tab.Screen name="Add Entry" component={AddEntry}/>
+      <Tab.Screen name="Live" component={Live}/>
+    </Tab.Navigator>
+  )
+}
+
+const Stack = createStackNavigator();
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+      name="Home" 
+      component={TabNaviagtor} 
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="Entry Detail" 
+      component={EntryDetail} 
+      options={
+        ({route}) => ({
+          title: route.params.entryId, 
+          headerTintColor: white, 
+          headerStyle: {backgroundColor: purple}
+        })
+      }/>
+    </Stack.Navigator>
+  );
+}
+
+export default class App extends Component {
+  componentDidMount(){
+    // Ensure to clear preset async storage values first
+    setLocalNotifications()
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+          <NavigationContainer>
+            <StackNavigator />
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
 }
